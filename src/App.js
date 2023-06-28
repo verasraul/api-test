eddb3c78746bb3301f6a9d5173263b09ac0ba375
Raul Veras <verasraul@gmail.com>
2023-06-27 20:22:14 -0400

    updaint app.js module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+
 function App() {
+
+const [data, setData] = useState({ hits: [] })
+ 
+  
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://hn.algolia.com/api/v1/search?query=redux');
+      console.log(result)
+       setData(result.data)
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <ul>
+      {data.hits.map((item) => {
+        //keys
+        //React requires a unique key for each element
+      return  <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      })
+      }
+   </ul>
   );
 }
 
